bf5d3aee00bcf273cd0497954c50f517c29ece93
安井 利紗 <yasui05821@gmail.com>
2016-05-27 19:00:22 +0900

    シェアボタン実装

--- frontend/assets/js/common.js
+++ frontend/assets/js/common.js
@@ -4,22 +4,6 @@ $(function() {
             $('body, html').animate({scrollTop: 0}, 500);
     });
 
-    $('.sns-tw-btn').click(function () {
-        $(".twitter-share-button a").click();
-    });
-
-
 });
 
-
-
-
-//Facebook
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.0";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
