c5d4a5122a0a16c24a729abd2e0593543c8894cb
安井 利紗 <yasui05821@gmail.com>
2016-05-27 16:00:15 +0900

    ファビコン、OGP画像作成

--- frontend/assets/js/common.js
+++ frontend/assets/js/common.js
@@ -1,7 +1,25 @@
 //page top
 $(function() {
     $('#pagetop-block').click(function () {
-        console.log("hogehoge")
             $('body, html').animate({scrollTop: 0}, 500);
     });
+
+    $('.sns-tw-btn').click(function () {
+        $(".twitter-share-button a").click();
+    });
+
+
 });
+
+
+
+
+//Facebook
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.0";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
