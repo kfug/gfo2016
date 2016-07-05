40368e15be77337616884e4253d7f6583c68c3a6
mikakane <mikakane2@gmail.com>
2016-07-05 15:51:28 +0900

    fix_staff_click

--- frontend/assets/js/common.js
+++ frontend/assets/js/common.js
@@ -3,6 +3,15 @@ $(function() {
     $('#pagetop-block').click(function () {
             $('body, html').animate({scrollTop: 0}, 500);
     });
+
+    $("#staff-container .staff").on("click",(e)=>{
+        console.log("hogehgeo")
+        e.preventDefault();
+        var $this = $(e.currentTarget);
+        var url = $this.find("a").attr("href");
+        window.open().location.href = url;
+    })
+
     new WOW().init();
 });
 
