ed9831e7e8d838e1d159720c9ecbd9ac7b930d55
安井 利紗 <yasui05821@gmail.com>
2016-09-23 15:09:53 +0900

    スタッフブログ２つ追加

--- frontend/data/staffBlog.js
+++ frontend/data/staffBlog.js
@@ -1,6 +1,12 @@
 module.exports = [
     {
         link: "http://tec-kansai.tech/%E6%9C%AA%E5%88%86%E9%A1%9E-2400.php"
+    },
+    {
+        link: "http://44-consol.hateblo.jp/entry/2016/09/22/151128"
+    },
+    {
+        link: "http://tec-kansai.tech/community/kfug-2481.php"
     }
-]
+];
 
